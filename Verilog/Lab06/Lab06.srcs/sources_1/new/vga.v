d98eafbfc0a09160a3a0746f485b2a6ea2379a39
NithiP <the-px@hotmail.com>
2020-05-14 23:06:38 +0700

    Update vga.v

--- Verilog/Lab06/Lab06.srcs/sources_1/new/vga.v
+++ Verilog/Lab06/Lab06.srcs/sources_1/new/vga.v
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/31/2019 03:31:32 PM
-// Design Name: 
+// Company: Bee's Group
+// Engineer: Bee's Group
+//
+// Create Date: 05/14/2020 03:31:32 PM
+// Design Name: Bee's Group
 // Module Name: vga
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
+// Project Name: Bee's Group
+// Target Devices: Bee's Group
+// Tool Versions: Bee's Group
+// Description: Bee's Group
+//
+// Dependencies: Bee's Group
+//
+// Revision: Bee's Group
 // Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Additional Comments: Bee's Group
+//
 //////////////////////////////////////////////////////////////////////////////////
 module vga_sync
 	(
@@ -24,7 +24,7 @@ module vga_sync
 		output wire hsync, vsync, video_on, p_tick,
 		output wire [9:0] x, y
 	);
-	
+
 	// constant declarations for VGA sync parameters
 	localparam H_DISPLAY       = 640; // horizontal display area
 	localparam H_L_BORDER      =  48; // horizontal left border
@@ -33,7 +33,7 @@ module vga_sync
 	localparam H_MAX           = H_DISPLAY + H_L_BORDER + H_R_BORDER + H_RETRACE - 1;
 	localparam START_H_RETRACE = H_DISPLAY + H_R_BORDER;
 	localparam END_H_RETRACE   = H_DISPLAY + H_R_BORDER + H_RETRACE - 1;
-	
+
 	localparam V_DISPLAY       = 480; // vertical display area
 	localparam V_T_BORDER      =  10; // vertical top border
 	localparam V_B_BORDER      =  33; // vertical bottom border
@@ -41,29 +41,29 @@ module vga_sync
 	localparam V_MAX           = V_DISPLAY + V_T_BORDER + V_B_BORDER + V_RETRACE - 1;
         localparam START_V_RETRACE = V_DISPLAY + V_B_BORDER;
 	localparam END_V_RETRACE   = V_DISPLAY + V_B_BORDER + V_RETRACE - 1;
-	
+
 	// mod-4 counter to generate 25 MHz pixel tick
 	reg [1:0] pixel_reg;
 	wire [1:0] pixel_next;
 	wire pixel_tick;
-	
+
 	always @(posedge clk, posedge reset)
 		if(reset)
 		  pixel_reg <= 0;
 		else
 		  pixel_reg <= pixel_next;
-	
-	assign pixel_next = pixel_reg + 1; // increment pixel_reg 
-	
+
+	assign pixel_next = pixel_reg + 1; // increment pixel_reg
+
 	assign pixel_tick = (pixel_reg == 0); // assert tick 1/4 of the time
-	
+
 	// registers to keep track of current pixel location
 	reg [9:0] h_count_reg, h_count_next, v_count_reg, v_count_next;
-	
+
 	// register to keep track of vsync and hsync signal states
 	reg vsync_reg, hsync_reg;
 	wire vsync_next, hsync_next;
- 
+
 	// infer registers
 	always @(posedge clk, posedge reset)
 		if(reset)
@@ -80,30 +80,30 @@ module vga_sync
                     vsync_reg   <= vsync_next;
                     hsync_reg   <= hsync_next;
 		    end
-			
+
 	// next-state logic of horizontal vertical sync counters
 	always @*
 		begin
-		h_count_next = pixel_tick ? 
+		h_count_next = pixel_tick ?
 		               h_count_reg == H_MAX ? 0 : h_count_reg + 1
 			       : h_count_reg;
-		
-		v_count_next = pixel_tick && h_count_reg == H_MAX ? 
-		               (v_count_reg == V_MAX ? 0 : v_count_reg + 1) 
+
+		v_count_next = pixel_tick && h_count_reg == H_MAX ?
+		               (v_count_reg == V_MAX ? 0 : v_count_reg + 1)
 			       : v_count_reg;
 		end
-		
+
         // hsync and vsync are active low signals
         // hsync signal asserted during horizontal retrace
         assign hsync_next = h_count_reg >= START_H_RETRACE
                             && h_count_reg <= END_H_RETRACE;
-   
+
         // vsync signal asserted during vertical retrace
-        assign vsync_next = v_count_reg >= START_V_RETRACE 
+        assign vsync_next = v_count_reg >= START_V_RETRACE
                             && v_count_reg <= END_V_RETRACE;
 
         // video only on when pixels are in both horizontal and vertical display region
-        assign video_on = (h_count_reg < H_DISPLAY) 
+        assign video_on = (h_count_reg < H_DISPLAY)
                           && (v_count_reg < V_DISPLAY);
 
         // output signals
@@ -122,13 +122,21 @@ module vga_test
 		output wire hsync, vsync,
 		output wire [11:0] rgb,
 		input wire[7:0] char,
-		output reg[15:0] led 
+		output reg[15:0] led
 	);
 
 	parameter WIDTH = 640;
 	parameter HEIGHT = 480;
-		
-	// register for Basys 2 8-bit RGB DAC 
+
+	//health
+	reg [1:0] hero = 3;
+	reg [1:0] mon1 = 3;
+	reg [1:0] mon2 = 3;
+
+	//state controller
+	reg [2:0] state = 0;
+
+	// register for Basys 2 8-bit RGB DAC
 	reg [11:0] rgb_reg;
 	reg reset = 0;
 	wire [9:0] x, y;
@@ -138,7 +146,7 @@ module vga_test
     wire p_tick;
 	// instantiate vga_sync
 	vga_sync vga_sync_unit (.clk(clk), .reset(reset), .hsync(hsync), .vsync(vsync), .video_on(video_on), .p_tick(p_tick), .x(x), .y(y));
-	
+
 	// rgb circle
 	reg[9:0] h, k;
 	reg[18:0] d_sq;
@@ -147,15 +155,40 @@ module vga_test
         h = 320;
         k = 240;
     end
+
     always @(posedge clk) begin
-        if( ( (x-h)*(x-h) + (y-k)*(y-k) ) <= 10000) begin
-            rgb_reg = sel_color;
-        end 
-        else begin
-            rgb_reg = 12'b000000000000;
-        end
+			//start_bee
+
+			//end_bee
+
+
+			//start_taan
+
+			//end_taan
+
+
+			//start_tee
+
+			//end_tee
+
+
+			//start_pud
+
+			//end_pud
+
+
+			//start_ou
+
+			//end_ou
+
+//        if( ( (x-h)*(x-h) + (y-k)*(y-k) ) <= 10000) begin
+//            rgb_reg = sel_color;
+//        end
+//        else begin
+//            rgb_reg = 12'b000000000000;
+//        end
     end
-            
+
 	always @(posedge clk) begin
 	    case(char)
 	       8'h00: led[8] = 1;
@@ -170,7 +203,7 @@ module vga_test
 	       default: led = 0;
 	    endcase
 	end
-	        
+
 //	output
 	assign rgb = (video_on) ? rgb_reg : 12'b0;
 endmodule
